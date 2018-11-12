112a63808697e90b5172924c5720abe89e5a527d
freepaas <1025815097@qq.com>
2018-11-12 16:38:24 +0800

    * 修改rocketmq测试用例的头文件

--- springboot-rocketmq/src/main/java/com/newqur/springbootrocketmq/SpringbootRocketmqApplication.java
+++ springboot-rocketmq/src/main/java/com/newqur/springbootrocketmq/SpringbootRocketmqApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * 学习https://www.jianshu.com/p/5260a2739d80
+ * 学习
+ * https://www.jianshu.com/p/5260a2739d80
+ * https://rocketmq.apache.org/docs/quick-start/
  */
 @SpringBootApplication
 public class SpringbootRocketmqApplication {
